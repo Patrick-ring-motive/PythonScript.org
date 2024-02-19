8832fc41afd500dde724c90938e0fcc128cc5d4d
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-02-19 08:17:56 -0600

    Update fillins.js

--- public/js/fillins.js
+++ public/js/fillins.js
@@ -1,9 +1,9 @@
 globalThis.debug = true;
 
 if(debug){
-  if(!globalThis['console.log']){globalThis['console.log'] = console.log;}
-  if(!globalThis['console.debug'])globalThis['console.debug'] = console.debug;}
-  if(!globalThis['console.error']) globalThis['console.error'] = console.error;}
+  if(!globalThis['console.log']){ globalThis['console.log'] = console.log; }
+  if(!globalThis['console.debug']){ globalThis['console.debug'] = console.debug; }
+  if(!globalThis['console.error']){ globalThis['console.error'] = console.error; }
   console.log = alert;
   console.debug = alert;
   console.error = alert;
