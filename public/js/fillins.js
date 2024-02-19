a294ecb7b58736c62053293874440030eafa1d34
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-02-19 08:13:28 -0600

    Update fillins.js

--- public/js/fillins.js
+++ public/js/fillins.js
@@ -1,3 +1,10 @@
+globalThis.debug = true;
+
+if(debug){
+  globalThis['console.log'] = console.log;
+  console.log = alert;
+}
+
 if(window){
   Object.defineProperty(window, 'nodeName', {
   value: 'window',
